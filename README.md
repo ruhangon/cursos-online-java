c1ee11b690734db79544b029131814c851a6fc66
Ruhan Gonçalves <ruhan.goncalves@gmail.com>
2021-08-21 10:18:13 -0300

    Implementa novos testes, incluindo testes em listas

--- README.md
+++ README.md
@@ -2,17 +2,3 @@
 
 Usado para estudos sobre java
 
-## Cursos feitos da plataforma Alura
-
-- Java parte 1: Primeiros passos
-
-- Java parte 2: Introdução à Orientação a Objetos
-
-- Java parte 3: Entendendo herança e interface
-
-- Java parte 4: Entendendo exceções
-
-- Java parte 5: Pacotes e java.lang
-
-- Java e java.util: Coleções, Wrappers e Lambda expressions
-
--- java-testes/testes-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
+++ java-testes/testes-de-unidade/src/br/com/caelum/leilao/servico/Avaliador.java
@@ -1,5 +1,8 @@
 package br.com.caelum.leilao.servico;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import br.com.caelum.leilao.dominio.Lance;
 import br.com.caelum.leilao.dominio.Leilao;
 
@@ -7,6 +10,7 @@ public class Avaliador {
 	private double maiorDeTodos = Double.NEGATIVE_INFINITY;
 	private double menorDeTodos = Double.POSITIVE_INFINITY;
 	private double mediaDosLances;
+	private List<Lance> maioresLances;
 
 	public void avalia(Leilao leilao) {
 		for (Lance lance : leilao.getLances()) {
@@ -15,12 +19,29 @@ public class Avaliador {
 			if (lance.getValor() < menorDeTodos)
 				menorDeTodos = lance.getValor();
 		}
+		if (leilao.getLances().isEmpty()) {
+			maiorDeTodos = 0;
+			menorDeTodos = 0;
+		}
+		descobreOsMaioresLances(leilao);
 	}
 
 	public void encontraMedia(Leilao leilao) {
 		if (!leilao.getLances().isEmpty()) {
 			double somaDosLances = leilao.getLances().stream().mapToDouble(x -> x.getValor()).sum();
 			mediaDosLances = somaDosLances / leilao.getLances().size();
+		}
+	}
+
+	public void descobreOsMaioresLances(Leilao leilao) {
+		List<Lance> lancesEmOrdem = new ArrayList<>(leilao.getLances());
+		maioresLances = new ArrayList<>();
+		lancesEmOrdem.sort((lance1, lance2) -> Double.compare(lance2.getValor(), lance1.getValor()));
+		int contTotalDeLances = 4;
+		if (leilao.getLances().size() < contTotalDeLances)
+			contTotalDeLances = leilao.getLances().size();
+		for (int contMaioresLances = 0; contMaioresLances < contTotalDeLances; contMaioresLances++) {
+			maioresLances.add(lancesEmOrdem.get(contMaioresLances));
 		}
 	}
 
@@ -34,6 +55,10 @@ public class Avaliador {
 
 	public double getMediaDosLances() {
 		return mediaDosLances;
+	}
+
+	public List<Lance> getMaioresLances() {
+		return maioresLances;
 	}
 
 }
